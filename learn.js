49bbccf7d402319c7d39df2c6c0eef0440a095eb
ricksan <gumballricksan.com>
2026-06-09 17:53:12 +0700

    HOF and Callback

---
+++ learn.js
@@ -0,0 +1,43 @@
+// function sambut() {
+// console.log("hello");
+// }
+
+// function eksekusiFungsi(func) {
+// console.log("before function");
+// func()
+// console.log("after function");
+// }
+
+// eksekusiFungsi(sambut)
+
+// function addNumbers(a, b) {
+//   return a + b;
+// }
+// const addNumbers = (a, b) => a + b;
+
+// function calculate(x, y, operation) {
+//   return operation(x, y);
+// }
+
+// const calculate = (x, y, operation) => operation(x, y);
+
+// const mathResult = calculate(15, 3, addNumbers);
+// console.log(mathResult);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+// for (i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+
+// }
+
+// numbers.forEach((num) => console.log(num));
+
+// const doubled = numbers.map((numb) => numb * 2);
+// const evens = numbers.map((numb) => numb % 2 === 0);
+
+// const firstEven = numbers.find((numb) => numb % 2 === 0);
+
+// const sum = numbers.reduce((total, num) => total + num, 0)
+
+// console.log(sum);
